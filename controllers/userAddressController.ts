4cb3afbf8e4c0365b16d791e611a3b667769e017
prernaorangeleague <prerna@orangeleague.in>
2024-06-23 10:38:33 +0530

    fetch address

--- controllers/userAddressController.ts
+++ controllers/userAddressController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import UserAddress from '../models/userAddressModel';
 
 export const addUserAddress = async (req: Request, res: Response) => {
-    const { userId, latitude , longitude, city, state, zipcode, address, landmark } = req.body;
+    const { userId, latitude, longitude, city, state, zipcode, address, landmark } = req.body;
 
     try {
         const userAddress = new UserAddress({
@@ -29,7 +29,26 @@ export const getUserAddressByUserId = async (req: Request, res: Response) => {
     const { userId } = req.query;
 
     try {
-       const addresses = await UserAddress.find({userId})
+        const addresses = await UserAddress.find({ userId })
+
+        if (addresses) {
+            res.status(201).json([]);
+        }
+        else {
+            res.status(201).json(addresses);
+
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+};
+
+export const getUserAddressById = async (req: Request, res: Response) => {
+    const { id } = req.query;
+
+    try {
+        const addresses = await UserAddress.find({ _id: id })
 
         res.status(201).json(addresses);
     } catch (error) {
