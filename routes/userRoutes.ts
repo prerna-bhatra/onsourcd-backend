d3c9b897bf115ec14418a178f4b182f8196bb6ba
prernaorangeleague <prerna@orangeleague.in>
2024-06-29 18:08:15 +0530

    verify email

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { registerUser, loginUser, verifyUserEmail } from '../controllers/userController';
+import { verifyTokenMiddleware } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/verify', verifyUserEmail);
+router.get('/verify',verifyTokenMiddleware, verifyUserEmail);
 
 export default router;
