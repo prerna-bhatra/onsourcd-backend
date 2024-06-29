d2382cdcae67e5a86b422648f36d7a7bddb51387
prernaorangeleague <prerna@orangeleague.in>
2024-06-29 18:05:08 +0530

    verify

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/verify', verifyUserEmail);
+router.get('/verify', verifyUserEmail);
 
 export default router;
