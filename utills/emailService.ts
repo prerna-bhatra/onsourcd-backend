9594fb4edc153e3c7c002a8006f6c38250ac6a0e
prernaorangeleague <prerna@orangeleague.in>
2024-06-30 09:44:26 +0530

    email verfy

--- utills/emailService.ts
+++ utills/emailService.ts
@@ -27,7 +27,7 @@ export const sendVerificationEmail = async (token:string , email:string) => {
     html: `
       <h1>Email Verification</h1>
       <p>Please verify your email by clicking the link below:</p>
-      <a href="http://localhost:3000/customer/verified?token=${token}">Verify Email</a>
+      <a href="http://localhost:3000/verified?token=${token}">Verify Email</a>
     `,
   };
 
